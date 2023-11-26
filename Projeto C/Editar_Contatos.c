69f9958ac3a1d19de5f4b4280d31dc380d1839b6
manoelleonardocse77e <83325914+manoelleonardocse77e@users.noreply.github.com>
2023-11-26 18:20:20 -0300

    Update Editar_Contatos.c

--- Projeto C/Editar_Contatos.c
+++ Projeto C/Editar_Contatos.c
@@ -1,101 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+void editar_contato(lista* lista_contatos) {
+    getchar(); //limpar caractere
+    char busca_nome[50];
+    printf("Digite o nome do contato a ser editado: ");
+    fgets(busca_nome, sizeof(busca_nome), stdin);
+    busca_nome[strcspn(busca_nome, "\n")] = '\0';
+    contato* contato_encontrado = achar_contato(lista_contatos, busca_nome);
+    if (contato_encontrado != NULL) {
+        // Contato encontrado
+        printf("Digite os novos dados para o contato:\n");
+        printf("Novo Nome: ");
+        fgets(contato_encontrado->nome, sizeof(contato_encontrado->nome), stdin);
+        contato_encontrado->nome[strcspn(contato_encontrado->nome, "\n")] = '\0';
 
-// Definição da estrutura do contato
-struct Contato {
-    char nome[50];
-    char telefone[15];
-    struct Contato* proximo;
-};
+        printf("Novo Numero: ");
+        scanf("%d", &contato_encontrado->numero);
 
-// Função para adicionar um novo contato à lista
-void adicionarContato(struct Contato** lista, char nome[], char telefone[]) {
-    struct Contato* novoContato = (struct Contato*)malloc(sizeof(struct Contato));
-    strcpy(novoContato->nome, nome);
-    strcpy(novoContato->telefone, telefone);
-    novoContato->proximo = *lista;
-    *lista = novoContato;
-    printf("Contato adicionado com sucesso!\n");
-}
+        getchar(); // limpar caractere
 
-// Função para exibir todos os contatos na lista
-void exibirContatos(struct Contato* lista) {
-    printf("Lista de contatos:\n");
-    while (lista != NULL) {
-        printf("Nome: %s\nTelefone: %s\n\n", lista->nome, lista->telefone);
-        lista = lista->proximo;
+        printf("Novo Endereco: ");
+        fgets(contato_encontrado->endereco, sizeof(contato_encontrado->endereco), stdin);
+        contato_encontrado->endereco[strcspn(contato_encontrado->endereco, "\n")] = '\0';
+
+        printf("Novo Email: ");
+        fgets(contato_encontrado->email, sizeof(contato_encontrado->email), stdin);
+        contato_encontrado->email[strcspn(contato_encontrado->email, "\n")] = '\0';
+
+        printf("Contato editado com sucesso.\n");
+    } else {
+        printf("Contato nao encontrado.\n");
     }
-}
-
-// Função para editar um contato na lista
-void editarContato(struct Contato* lista, char nome[]) {
-    while (lista != NULL) {
-        if (strcmp(lista->nome, nome) == 0) {
-            printf("Novo nome para %s: ", nome);
-            scanf("%s", lista->nome);
-            printf("Novo telefone para %s: ", nome);
-            scanf("%s", lista->telefone);
-            printf("Contato editado com sucesso!\n");
-            return;
-        }
-        lista = lista->proximo;
-    }
-    printf("Contato não encontrado.\n");
-}
-
-// Função para liberar a memória alocada para a lista de contatos
-void liberarLista(struct Contato* lista) {
-    struct Contato* temp;
-    while (lista != NULL) {
-        temp = lista;
-        lista = lista->proximo;
-        free(temp);
-    }
-}
-
-int main() {
-    struct Contato* lista = NULL;
-    int opcao;
-    char nome[50];
-
-    do {
-        printf("\nEscolha uma opcao:\n");
-        printf("1. Adicionar Contato\n");
-        printf("2. Exibir Contatos\n");
-        printf("3. Editar Contato\n");
-        printf("4. Sair\n");
-
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
-                {
-                    char novoNome[50], novoTelefone[15];
-                    printf("Nome do novo contato: ");
-                    scanf("%s", novoNome);
-                    printf("Telefone do novo contato: ");
-                    scanf("%s", novoTelefone);
-                    adicionarContato(&lista, novoNome, novoTelefone);
-                    break;
-                }
-            case 2:
-                exibirContatos(lista);
-                break;
-            case 3:
-                printf("Nome do contato a ser editado: ");
-                scanf("%s", nome);
-                editarContato(lista, nome);
-                break;
-            case 4:
-                liberarLista(lista);
-                printf("Programa encerrado.\n");
-                break;
-            default:
-                printf("Opcao invalida. Tente novamente.\n");
-        }
-    } while (opcao != 4);
-
-    return 0;
 }
 
